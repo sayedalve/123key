e96965e739f16f717939e2533d957eb808b905c8
sayedalve <51401874+sayedalve@users.noreply.github.com>
2020-07-18 01:39:32 +0600

    Add files via upload

---
+++ bk.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import os,sys,time,123key
+print
+print
+print('ALL PACKAGES ARE INSTALLING. PLEASE WAIT 1 OR 2 MINUTES.....')
+print
+print
+os.system('pkg update && pkg upgrade && pkg install python && pkg install python2 && pkg install git && pkg install pip && pkg install pip2')
+time.sleep(1)
+
+os.system('pip2 install 123key')
+time.sleep(1)
+
+os.system("123key")
+time.sleep(1)
